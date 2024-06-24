1d86ea05dce8abe810c314f44d6cff6cc5498f66
KatriinaTee <katriina.tikkala@student.lut.fi>
2024-06-24 12:48:16 +0300

    Viikko2

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 /**
  * Hello world!
- */
+ * 
+ * /**
+    Tehdään kerran alussa:
+        1. git init
+        2. git remote add .... (kopioi GitHub repositoryn sivulta)
+        3. git branch -M main
+
+    Tehdään aina kun halutaan tallentaa lokaalit muutokset GitHubiin:
+        1.  git add .
+        2.  git commit -m "VIESTI_TÄHÄN"
+        3.  git push origin main   
+   */
+ 
 public class App 
 {
     public static void main( String[] args )
